1254bf4ba435c490bbc4b15042985c4f9c2e71c2
John Hur <hur.john628@gmail.com>
2021-11-03 18:53:35 -0500

    testing bing map api for streetview

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    /* No need to set credentials if already passed in URL */
+    mapTypeId: Microsoft.Maps.MapTypeId.road,
+    zoom: 18,
+    center: new Microsoft.Maps.Location(28.332823, -81.492279)
+});
+map.setView({ mapTypeId: Microsoft.Maps.MapTypeId.streetside });
